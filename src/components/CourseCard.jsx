48647ab5143886cee3833d1f670005034c2373d2
Theresalin39 <90170508+Theresalin39@users.noreply.github.com>
2023-12-11 12:56:53 -0800

    course list

---
+++ src/components/CourseCard.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const CourseCard = ({ course }) => {
+    // Random color for the course title on the thumbnail
+    const colors = ['text-red-500', 'text-blue-500', 'text-green-500', 'text-yellow-500'];
+    const randomColor = colors[Math.floor(Math.random() * colors.length)];
+
+    return (
+        <div className="bg-white rounded-lg shadow-md p-4">
+            <div className="bg-black text-white rounded-lg p-6 flex items-center justify-center">
+                <h2 className={`text-2xl font-bold ${randomColor}`}>{course.title}</h2>
+            </div>
+            <div className="mt-4">
+                <p className="text-lg">{course.description}</p>
+                <p className="mt-2 font-semibold">Price: ${course.price}</p>
+            </div>
+        </div>
+    );
+};
+
+export default CourseCard;
---
+++ src/pages/courses/index.jsx
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from 'react';
+import supabase from '../../lib/supabase'; // Adjust the path as necessary
+import CourseCard from '../../components/CourseCard';
+
+const CoursesPage = () => {
+    const [courses, setCourses] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const fetchCourses = async () => {
+            const { data, error } = await supabase
+                .from('courses')
+                .select('*');
+
+            if (error) {
+                console.error('Error fetching courses:', error);
+                return;
+            }
+
+            setCourses(data);
+            setLoading(false);
+        };
+
+        fetchCourses();
+    }, []);
+
+    if (loading) {
+        return <div>Loading courses...</div>;
+    }
+
+    return (
+        <div className="container mx-auto p-4">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+                {courses.map(course => (
+                    <CourseCard key={course.id} course={course} />
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default CoursesPage;

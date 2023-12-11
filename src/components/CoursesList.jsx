54531486cbd3dbd6677b3dd7499684bee0af1054
Theresalin39 <90170508+Theresalin39@users.noreply.github.com>
2023-12-10 22:32:16 -0800

    second commit

---
+++ src/components/CoursesList.jsx
@@ -0,0 +1,50 @@
+
+import React, { useEffect, useState } from 'react';
+import supabase from '../lib/supabase'; // Adjust the path according to your project structure
+
+const CoursesList = () => {
+    const [courses, setCourses] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const fetchCourses = async () => {
+            const { data, error } = await supabase
+                .from('courses')
+                .select('*');
+
+            if (error) {
+                console.error('Error fetching courses:', error);
+                return;
+            }
+
+            setCourses(data);
+            setLoading(false);
+        };
+
+        fetchCourses();
+    }, []);
+
+    if (loading) {
+        return <div>Loading courses...</div>;
+    }
+
+    return (
+        <div>
+            {courses.length === 0 ? (
+                <div>No courses available.</div>
+            ) : (
+                courses.map(course => (
+                    <div key={course.id} className="course">
+                        <h3>{course.title}</h3>
+                        <p>{course.description}</p>
+                        <p>Price: ${course.price}</p>
+                        {course.thumbnail_url && <img src={course.thumbnail_url} alt={course.title} />}
+                        {/* You can add more course details here */}
+                    </div>
+                ))
+            )}
+        </div>
+    );
+};
+
+export default CoursesList;

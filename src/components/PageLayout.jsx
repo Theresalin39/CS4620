e9aba413092a69756ca340896be8fbe0c783bad6
Theresalin39 <90170508+Theresalin39@users.noreply.github.com>
2023-12-13 17:04:01 -0800

    adjust search input box

--- src/components/PageLayout.jsx
+++ src/components/PageLayout.jsx
@@ -2,34 +2,35 @@ import React, { useState } from 'react';
 
 const PageLayout = ({ title, showSearchBar, onSearch }) => {
     const [searchTerm, setSearchTerm] = useState('');
-  
+
     const handleSearch = (term) => {
-      // Trigger the search when the user presses the Enter key
-      onSearch(term);
+        // Trigger the search when the user presses the Enter key
+        onSearch(term);
     };
-  
+
     const handleKeyPress = (e) => {
-      if (e.key === 'Enter') {
-        handleSearch(searchTerm);
-      }
+        if (e.key === 'Enter') {
+            handleSearch(searchTerm);
+        }
     };
-  
+
     return (
-      <div className="container mx-auto px-4 py-4">
-        <h1 className="text-4xl font-extrabold text-gray-800 mb-4">{title}</h1>
-        {showSearchBar && (
-          <input
-            type="text"
-            className="w-full px-4 py-2 border border-gray-300 rounded-md focus:ring-gray-500 focus:border-gray-500"
-            placeholder="Search courses..."
-            value={searchTerm}
-            onChange={(e) => setSearchTerm(e.target.value)}
-            onKeyPress={handleKeyPress}
-          />
-        )}
-      </div>
+        <div className="container mx-auto px-4 py-4">
+            {showSearchBar && (
+                <div className="flex justify-end mb-4">
+                    <input
+                        type="text"
+                        className="w-2/5 px-4 py-2 border border-gray-300 rounded-md focus:ring-gray-500 focus:border-gray-500"
+                        placeholder="Search courses..."
+                        value={searchTerm}
+                        onChange={(e) => setSearchTerm(e.target.value)}
+                        onKeyPress={handleKeyPress}
+                    />
+                </div>
+            )}
+            <h1 className="text-4xl font-extrabold text-gray-800">{title}</h1>
+        </div>
     );
-  };
-  
+};
 
 export default PageLayout;
